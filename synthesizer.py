e84f3bde85181e03da184751374d7e8cb14e61c9
Matthias Geier <Matthias.Geier@gmail.com>
2020-07-14 11:55:53 +0200

    alternative implementation

--- synthesizer.py
+++ synthesizer.py
@@ -1,104 +1,149 @@
 #!pypy3
-import math
-import random
 import threading
 
 import numpy as np
-import matplotlib.pyplot as plt
 import sounddevice
 import mido
 
-CUTOFF_FACTOR = 6
-MASTER_VOLUME = 0.1
+
+amplitude = 0.1
+attack_seconds = 0.01
+release_seconds = 0.2
 
 
-class Audio:
-    def play(self, samplerate=8000):
-        time_array = np.linspace(0, self.length, int(self.length * samplerate))
-        pressure_array = np.zeros(time_array.shape, dtype='d')
+def m2f(note):
+    """Convert MIDI note number to frequency in Hertz.
 
-        playing_thread = threading.Thread(
-            target=lambda: sounddevice.play(
-                pressure_array, samplerate=samplerate, blocking=True
-            ),
-            daemon=True
-        )
-        playing_thread.start()
+    See https://en.wikipedia.org/wiki/MIDI_Tuning_Standard.
 
-        for i, t in enumerate(time_array):
-            pressure_array[i] = self.get_pressure(t.item())
-
-        playing_thread.join()
+    """
+    return 2 ** ((note - 69) / 12) * 440
 
 
-class Note(Audio):
-    overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
-    half_life = 0.3
+def update_envelope(envelope, begin, target, vel):
+    """Helper function to calculate envelopes.
 
-    def __init__(self, frequency, volume=1):
-        self.frequency = frequency
-        self.volume = volume
-        self.length = self.half_life * CUTOFF_FACTOR
+    envelope: array of velocities, will be mutated
+    begin: sample index where ramp begins
+    target: sample index where *vel* shall be reached
+    vel: final velocity value
+    If the ramp goes beyond the blocksize, it is supposed to be
+    continued in the next block.
+    A reference to *envelope* is returned, as well as the (unchanged)
+    *vel* and the target index of the following block where *vel* shall
+    be reached.
 
-    def get_pressure(self, t):
-        if (0 <= t <= self.length):
-            result = 0
-            for overtone, overtone_volume in self.overtones.items():
-                result += overtone_volume * math.sin(
-                    math.tau * t * self.frequency * overtone
-                )
-            volume = (MASTER_VOLUME * self.volume *
-                      2 ** (-t / self.half_life))
-            result *= volume
-            return result
+    """
+    blocksize = len(envelope)
+    old_vel = envelope[begin]
+    slope = (vel - old_vel) / (target - begin + 1)
+    ramp = np.arange(min(target, blocksize) - begin) + 1
+    envelope[begin:target] = ramp * slope + old_vel
+    if target < blocksize:
+        envelope[target:] = vel
+        target = 0
+    else:
+        target -= blocksize
+    return envelope, vel, target
+
+
+def play_midi_file(path, samplerate=48000, blocksize=1024, **kwargs):
+    assert blocksize > 0
+
+    midi_stream = iter(mido.MidiFile(path))
+
+    for msg in midi_stream:
+        if msg.type == 'note_on':
+            break
+        print('ignoring', msg.dict())
+
+    offset = 0
+    current_frame = 0
+    voices = {}
+
+    attack = int(attack_seconds * samplerate)
+    release = int(release_seconds * samplerate)
+
+    def callback(outdata, frames, time, status):
+        nonlocal msg
+        nonlocal offset
+        nonlocal current_frame
+
+        assert blocksize == frames
+        if status:
+            print(status)
+
+        # Step 1: Update/delete existing voices from previous block
+
+        # Iterating over a copy because items may be deleted:
+        for pitch in list(voices):
+            envelope, vel, target = voices[pitch]
+            if any([vel, target]):
+                envelope[0] = envelope[-1]
+                voices[pitch] = update_envelope(envelope, 0, target, vel)
+            else:
+                del voices[pitch]
+
+        # Step 2: Create envelopes from the MIDI events of the current block
+
+        if offset is None:
+            if not voices:
+                outdata.fill(0)
+                raise sounddevice.CallbackAbort()
         else:
-            return 0
+            while offset < frames:
+                if msg.type == 'note_on' and msg.velocity > 0:
+                    try:
+                        envelope, _, _ = voices[msg.note]
+                    except KeyError:
+                        envelope = np.zeros(frames)
+                    voices[msg.note] = update_envelope(
+                        envelope, offset, offset + attack, msg.velocity)
+                elif msg.type in ('note_on', 'note_off'):
+                    # NoteOff velocity is ignored!
+                    try:
+                        envelope, _, _ = voices[msg.note]
+                    except KeyError:
+                        print('NoteOff without NoteOn (ignored)')
+                    else:
+                        voices[msg.note] = update_envelope(
+                            envelope, offset, offset + release, 0)
+                else:
+                    print('ignoring', msg.dict())
 
-class Sequence(Audio):
-    def __init__(self, members):
-        self.members = tuple(members)
-        self.length = max(
-            offset + member.length for offset, member in self.members
-        )
+                try:
+                    msg = next(midi_stream)
+                except StopIteration:
+                    offset = None
+                    break
+                offset += round(msg.time * samplerate)
+            if offset is not None:
+                offset -= frames
 
-    def get_pressure(self, t):
-        return sum(member.get_pressure(t - offset) for offset, member in
-                   self.members)
+        # Step 3: Create sine tones, apply envelopes, add to output buffer
 
+        outdata.fill(0)
+        for pitch, (envelope, _, _) in voices.items():
+            t = (np.arange(frames) + current_frame) / samplerate
+            tone = amplitude * sum(
+                1 / (i ** 1.5) * np.sin(2 * np.pi * m2f(pitch) * i * t)
+                for i in range(1, 8)
+            )
+            outdata[:, 0] += tone * envelope / 127
+        current_frame += frames
 
+    event = threading.Event()
 
-class MidiSequence(Sequence):
-    tempo = None
-
-    def __init__(self, path):
-        self.midi = mido.MidiFile(path)
-
-        members = []
-
-        meta_track, track, *_ = self.midi.tracks
-
-        tempo_messages = [message for message in meta_track if
-                          message.type == 'set_tempo']
-        self.tempo = tempo_messages[0].tempo
-
-        current_time = 0
-        for message in track:
-            if message.type not in {'note_on', 'note_off'}:
-                continue
-            time_interval = mido.tick2second(
-                message.time, self.midi.ticks_per_beat, self.tempo)
-            current_time += time_interval
-            members.append((
-                current_time,
-                Note(
-                    220 * 2 ** ((message.note - 57) / 12),
-                    message.velocity / 127
-                )
-            ))
-
-        Sequence.__init__(self, members)
+    stream = sounddevice.OutputStream(
+        channels=1,
+        blocksize=blocksize,
+        callback=callback,
+        samplerate=samplerate,
+        finished_callback=event.set,
+        **kwargs)
+    with stream:
+        event.wait()
 
 
 if __name__ == '__main__':
-    midi_sequence = MidiSequence('FurElise.mid')
-    midi_sequence.play()
+    play_midi_file('FurElise.mid')
