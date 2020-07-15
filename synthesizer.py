f3a277035aeeaab69ae1b644e79f4ef9239d5ab5
Matthias Geier <Matthias.Geier@gmail.com>
2020-07-15 11:46:04 +0200

    Re-implement with the rtmixer module

--- synthesizer.py
+++ synthesizer.py
@@ -1,79 +1,50 @@
-#!pypy3
-import math
-import random
-
-import numpy as np
-import sounddevice
 import mido
+import numpy as np
+import rtmixer
+import sounddevice as sd
 
 MASTER_VOLUME = 0.1
 
 
-class Audio:
-    def play(self, samplerate=48000, delay_seconds=0.2):
-        self.samplerate = samplerate
-        self.delay = int(delay_seconds * samplerate)
-        self.pressure_array = np.zeros(
-            int(self.length * samplerate) + self.delay, dtype='d')
-        sounddevice.play(self.pressure_array, samplerate=self.samplerate)
-        self.update_pressure()
-        sounddevice.wait()
+class Player(rtmixer.Mixer):
+
+    def play_note(self, frequency, *, time, volume=1):
+        overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
+        half_life = 0.3
+        duration = 1.5
+        fs = self.samplerate
+        t = np.arange(int(duration * fs), dtype='float32') / fs
+        buffer = sum(
+            overtone_volume * np.sin(2 * np.pi * t * frequency * overtone)
+            for overtone, overtone_volume in overtones.items())
+        buffer *= MASTER_VOLUME * volume * 2 ** (-t / half_life)
+        while len(self.actions) > 20:
+            # Don't calculate all notes at once (apply backpressure)
+            sd.sleep(100)
+        assert buffer.dtype == np.float32
+        assert buffer.flags.c_contiguous == True
+        return self.play_buffer(
+            buffer, channels=1, start=time, allow_belated=False)
 
 
-class Note:
-
-    overtones = {i: 1 / (i ** 1.5) for i in range(1, 8)}
-    half_life = 0.3
-
-    def __init__(self, frequency, volume=1):
-        self.frequency = frequency
-        self.volume = volume
-        self.length = 1.5
-
-    def get_pressure(self, samplerate):
-        t = np.arange(int(self.length * samplerate)) / samplerate
-        result = sum(
-            overtone_volume * np.sin(2 * np.pi * t * self.frequency * overtone)
-            for overtone, overtone_volume in self.overtones.items())
-        volume = (MASTER_VOLUME * self.volume * 2 ** (-t / self.half_life))
-        return result * volume
-
-
-class Sequence(Audio):
-    def __init__(self, members):
-        self.members = tuple(members)
-        self.length = max(
-            offset + member.length for offset, member in self.members
-        )
-
-    def update_pressure(self):
-        for offset, note in self.members:
-            pressure = note.get_pressure(self.samplerate)
-            offset = int(offset * self.samplerate) + self.delay
-            self.pressure_array[offset:offset + len(pressure)] += pressure
-
-
-class MidiSequence(Sequence):
-
-    def __init__(self, path):
-        members = []
-
-        current_time = 0
-        for message in mido.MidiFile(path):
+def play_midi(path):
+    midifile = mido.MidiFile(path)
+    with Player(channels=1) as p:
+        delay = 0.5
+        current_time = p.time + delay
+        for message in midifile:
             current_time += message.time
             if message.type != 'note_on':
                 continue
-            members.append((
-                current_time,
-                Note(
-                    220 * 2 ** ((message.note - 57) / 12),
-                    message.velocity / 127
-                )
-            ))
-
-        Sequence.__init__(self, members)
+            p.play_note(
+                time=current_time,
+                frequency=220 * 2 ** ((message.note - 57) / 12),
+                volume=message.velocity / 127
+            )
+        # Wait for everything to be finished:
+        while p.actions:
+            sd.sleep(100)
 
 
 if __name__ == '__main__':
-    midi_sequence = MidiSequence('FurElise.mid')
-    midi_sequence.play()
+    play_midi('FurElise.mid')
